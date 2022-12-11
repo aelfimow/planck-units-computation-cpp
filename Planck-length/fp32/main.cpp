1592803a89b41ead44c976b2bf8977e9971632ee
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-11 20:23:05 +0100

    Planck-length: set precision

--- Planck-length/fp32/main.cpp
+++ Planck-length/fp32/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 
 using fp32 = float;
 static_assert(sizeof(fp32) == 4U);
@@ -8,16 +9,16 @@ static_assert(sizeof(fp32) == 4U);
 int main(int, char *[])
 {
     fp32 const pi = 3.1415926535897932384626433832795f;
-    fp32 const h = 6.62607015E-34;
-    fp32 const h_reduced = h / (2.0 * pi);
+    fp32 const h = 6.62607015E-34f;
+    fp32 const h_reduced = h / (2.0f * pi);
 
-    fp32 const G = 6.67430E-11;
+    fp32 const G = 6.67430E-11f;
 
     fp32 const c = 299792458.0f;
 
-    fp32 const lp = sqrt((h_reduced * G) / pow(c, 3.0f));
+    fp32 const lp = static_cast<fp32>(sqrt((h_reduced * G) / pow(c, 3.0f)));
 
-    std::cout << "Planck length (fp32): " << lp << std::endl;
+    std::cout << std::setprecision(10) << "Planck length (fp32): " << lp << std::endl;
 
     return EXIT_SUCCESS;
 }
