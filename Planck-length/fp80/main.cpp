a3ef38d3cce23b279a049c937c5f0f789b81b57e
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-12 19:37:27 +0100

    Planck-length: use proper suffix

--- Planck-length/fp80/main.cpp
+++ Planck-length/fp80/main.cpp
@@ -8,15 +8,15 @@ static_assert(sizeof(fp80) >= 10U);
 
 int main(int, char *[])
 {
-    fp80 const pi = 3.1415926535897932384626433832795;
-    fp80 const h = 6.62607015E-34;
-    fp80 const h_reduced = h / (2.0 * pi);
+    fp80 const pi = 3.1415926535897932384626433832795L;
+    fp80 const h = 6.62607015E-34L;
+    fp80 const h_reduced = h / (2.0L * pi);
 
-    fp80 const G = 6.67430E-11;
+    fp80 const G = 6.67430E-11L;
 
-    fp80 const c = 299792458.0;
+    fp80 const c = 299792458.0L;
 
-    fp80 const lp = sqrt((h_reduced * G) / pow(c, 3.0));
+    fp80 const lp = sqrtl((h_reduced * G) / pow(c, 3.0L));
 
     std::cout << std::setprecision(10) << "Planck length (fp80): " << lp << std::endl;
 
