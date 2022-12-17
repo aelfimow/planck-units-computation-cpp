a80319c3325aaaaed5f47e3b783288a00072c389
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-17 21:08:19 +0100

    Planck-time: renamed variable

--- Planck-time/fp128/main.cpp
+++ Planck-time/fp128/main.cpp
@@ -18,11 +18,11 @@ int main(int, char *[])
 
     fp128 const c = 299792458.0Q;
 
-    fp128 const lp = sqrtq((h_reduced * G) / powq(c, 5.0Q));
+    fp128 const T = sqrtq((h_reduced * G) / powq(c, 5.0Q));
 
     std::vector<char> buffer(256U);
 
-    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", lp);
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", T);
 
     std::string const lp_str(buffer.data());
 
