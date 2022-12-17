a593d740bb2b8d991f94b82877db0599df044566
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-17 21:09:53 +0100

    Planck-temperature: added files

---
+++ Planck-temperature/fp80/main.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using fp80 = long double;
+static_assert(sizeof(fp80) >= 10U);
+
+int main(int, char *[])
+{
+    fp80 const pi = 3.1415926535897932384626433832795L;
+    fp80 const h = 6.62607015E-34L;
+    fp80 const h_reduced = h / (2.0L * pi);
+
+    fp80 const G = 6.67430E-11L;
+
+    fp80 const c = 299792458.0L;
+
+    fp80 const kb = 1.380649E-23L;
+
+    fp80 const Tp = (sqrtl((h_reduced * c) / G) * powl(c, 2.0)) / kb;
+
+    std::cout << std::setprecision(10) << "Planck temperature (fp80): " << Tp << std::endl;
+
+    return EXIT_SUCCESS;
+}
