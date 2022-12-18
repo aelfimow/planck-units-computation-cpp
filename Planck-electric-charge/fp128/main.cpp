2149058a43ec86813b0bff3b2ba0c79f74071b99
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-18 18:59:46 +0100

    Planck-electric-charge: use proper pi

--- Planck-electric-charge/fp128/main.cpp
+++ Planck-electric-charge/fp128/main.cpp
@@ -10,15 +10,14 @@ static_assert(sizeof(fp128) == 16U);
 
 int main(int, char *[])
 {
-    fp128 const pi = 3.1415926535897932384626433832795Q;
     fp128 const h = 6.62607015E-34Q;
-    fp128 const h_reduced = h / (2.0Q * pi);
+    fp128 const h_reduced = h / (2.0Q * M_PIq);
 
     fp128 const c = 299792458.0Q;
 
     fp128 const eps0 = 8.8541878128E-12f;
 
-    fp128 const qp = sqrtq(4.0Q * pi * eps0 * h_reduced * c);
+    fp128 const qp = sqrtq(4.0Q * M_PIq * eps0 * h_reduced * c);
 
     std::vector<char> buffer(256U);
 
