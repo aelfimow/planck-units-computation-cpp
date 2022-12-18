437b25dcb204b36aa351e89f67d1fccefaa75a8e
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-18 18:56:05 +0100

    Planck-electric-charge: added files

---
+++ Planck-electric-charge/fp128/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <quadmath.h>
+
+using fp128 = __float128;
+static_assert(sizeof(fp128) == 16U);
+
+int main(int, char *[])
+{
+    fp128 const pi = 3.1415926535897932384626433832795Q;
+    fp128 const h = 6.62607015E-34Q;
+    fp128 const h_reduced = h / (2.0Q * pi);
+
+    fp128 const c = 299792458.0Q;
+
+    fp128 const eps0 = 8.8541878128E-12f;
+
+    fp128 const qp = sqrtq(4.0Q * pi * eps0 * h_reduced * c);
+
+    std::vector<char> buffer(256U);
+
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", qp);
+
+    std::string const lp_str(buffer.data());
+
+    std::cout << "Planck electric charge (fp128): " << lp_str << std::endl;
+
+    return EXIT_SUCCESS;
+}
