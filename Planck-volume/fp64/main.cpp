a66cb317e19bf96ce08d0a11955cbf6fdf8491e1
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-19 18:56:17 +0100

    Planck-volume: added files

---
+++ Planck-volume/fp64/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using fp64 = double;
+static_assert(sizeof(fp64) == 8U);
+
+int main(int, char *[])
+{
+    fp64 const pi = 3.1415926535897932384626433832795;
+    fp64 const h = 6.62607015E-34;
+    fp64 const h_reduced = h / (2.0 * pi);
+
+    fp64 const G = 6.67430E-11;
+
+    fp64 const c = 299792458.0;
+
+    fp64 const lp3 = pow(sqrt((h_reduced * G) / pow(c, 3.0)), 3.0);
+
+    std::cout << std::setprecision(10) << "Planck volume (fp64): " << lp3 << std::endl;
+
+    return EXIT_SUCCESS;
+}
