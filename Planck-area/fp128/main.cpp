dce9f59d385bbb8338406311e075c95b99aeacc8
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-19 18:45:11 +0100

    Planck-area: added files

---
+++ Planck-area/fp128/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <quadmath.h>
+
+using fp128 = __float128;
+static_assert(sizeof(fp128) == 16U);
+
+int main(int, char *[])
+{
+    fp128 const h = 6.62607015E-34Q;
+    fp128 const h_reduced = h / (2.0Q * M_PIq);
+
+    fp128 const G = 6.67430E-11Q;
+
+    fp128 const c = 299792458.0Q;
+
+    fp128 const lp2 = (h_reduced * G) / powq(c, 3.0Q);
+
+    std::vector<char> buffer(256U);
+
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", lp2);
+
+    std::string const lp_str(buffer.data());
+
+    std::cout << "Planck area (fp128): " << lp_str << std::endl;
+
+    return EXIT_SUCCESS;
+}
