f64431a4f876c377687a2721ed3454738b0d02dc
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-28 20:23:21 +0100

    Planck-force: added files

---
+++ Planck-force/fp128/main.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <quadmath.h>
+
+using fp128 = __float128;
+static_assert(sizeof(fp128) == 16U);
+
+int main(int, char *[])
+{
+    fp128 const G = 6.67430E-11Q;
+
+    fp128 const c = 299792458.0Q;
+
+    fp128 const Fp = powq(c, 4.0Q) / G;
+
+    std::vector<char> buffer(256U);
+
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", Fp);
+
+    std::string const lp_str(buffer.data());
+
+    std::cout << "Planck force (fp128): " << lp_str << std::endl;
+
+    return EXIT_SUCCESS;
+}
