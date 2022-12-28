5c3557597af344e5af2a16a842385719b7300dff
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-28 20:28:14 +0100

    Planck-momentum: renamed variable

--- Planck-momentum/fp128/main.cpp
+++ Planck-momentum/fp128/main.cpp
@@ -17,11 +17,11 @@ int main(int, char *[])
 
     fp128 const c = 299792458.0Q;
 
-    fp128 const Ep = sqrtq((h_reduced * powq(c, 3.0Q)) / G);
+    fp128 const momentum = sqrtq((h_reduced * powq(c, 3.0Q)) / G);
 
     std::vector<char> buffer(256U);
 
-    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", Ep);
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", momentum);
 
     std::string const lp_str(buffer.data());
 
