c0a79f8ab67c18aacf86b842a0e90b9315300ea6
Alexander Elfimow <my.free.mail@gmx.de>
2022-12-28 20:09:57 +0100

    Planck-momentum: added files

---
+++ Planck-momentum/fp80/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using fp80 = long double;
+static_assert(sizeof(fp80) >= 10U);
+
+int main(int, char *[])
+{
+    fp80 const pi = 3.1415926535897932384626433832795L;
+    fp80 const h = 6.62607015E-34L;
+    fp80 const h_reduced = h / (2.0L * pi);
+
+    fp80 const G = 6.67430E-11L;
+
+    fp80 const c = 299792458.0L;
+
+    fp80 const Ep = sqrtl((h_reduced * powl(c, 3.0L)) / G);
+
+    std::cout << std::setprecision(10) << "Planck momentum (fp80): " << Ep << std::endl;
+
+    return EXIT_SUCCESS;
+}
