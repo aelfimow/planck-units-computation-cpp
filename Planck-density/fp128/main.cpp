027481bed4d09feac8a19205f3cd7b4b313c406d
Alexander Elfimow <my.free.mail@gmx.de>
2023-01-11 19:48:25 +0100

    Planck-density: added files

---
+++ Planck-density/fp128/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <quadmath.h>
+
+using fp128 = __float128;
+static_assert(sizeof(fp128) == 16U);
+
+int main(int, char *[])
+{
+    fp128 const h = 6.62607015E-34Q;
+    fp128 const h_reduced = h / (2.0Q * M_PIq);
+
+    fp128 const G = 6.67430E-11Q;
+
+    fp128 const c = 299792458.0Q;
+
+    fp128 const rho = powq(c, 5.0Q) / (h_reduced * powq(G, 2.0Q));
+
+    std::vector<char> buffer(256U);
+
+    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.36Qe", rho);
+
+    std::string const lp_str(buffer.data());
+
+    std::cout << "Planck density (fp128): " << lp_str << std::endl;
+
+    return EXIT_SUCCESS;
+}
