feeab739ddcdf0e5745a985496f2cafa69795f53
Alexander Elfimow <my.free.mail@gmx.de>
2023-01-12 20:47:18 +0100

    Planck-pressure: added files

---
+++ Planck-pressure/fp32/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using fp32 = float;
+static_assert(sizeof(fp32) == 4U);
+
+int main(int, char *[])
+{
+    fp32 const pi = 3.1415926535897932384626433832795f;
+    fp32 const h = 6.62607015E-34f;
+    fp32 const h_reduced = h / (2.0f * pi);
+
+    fp32 const G = 6.67430E-11f;
+
+    fp32 const c = 299792458.0f;
+
+    fp32 const p = powf(c, 7.0f) / (h_reduced * powf(G, 2.0f));
+
+    std::cout << std::setprecision(10) << "Planck pressure (fp32): " << p << std::endl;
+
+    return EXIT_SUCCESS;
+}
