9695ea514b129f1b5a770717b0b9225db3e83124
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-10 18:20:33 +0100

    Planck-impedance: use pi from numbers

--- Planck-impedance/fp80/main.cpp
+++ Planck-impedance/fp80/main.cpp
@@ -2,19 +2,18 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <numbers>
 
 using fp80 = long double;
 static_assert(sizeof(fp80) >= 10U);
 
 int main(int, char *[])
 {
-    fp80 const pi = 3.1415926535897932384626433832795L;
-
     fp80 const c = 299792458.0L;
 
     fp80 const eps0 = 8.8541878128E-12L;
 
-    fp80 const Zp = 1.0L / (4.0L * pi * eps0 * c);
+    fp80 const Zp = 1.0L / (4.0L * std::numbers::pi_v<fp80> * eps0 * c);
 
     std::cout << std::setprecision(10) << "Planck impedance (fp80): " << Zp << std::endl;
 
