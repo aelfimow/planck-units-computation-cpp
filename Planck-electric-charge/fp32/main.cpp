8dee92eb29ad3695245d59655b9e522b04d10e56
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-10 18:20:32 +0100

    Planck-electric-charge: use pi from numbers

--- Planck-electric-charge/fp32/main.cpp
+++ Planck-electric-charge/fp32/main.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <numbers>
 
 using fp32 = float;
 static_assert(sizeof(fp32) == 4U);
 
 int main(int, char *[])
 {
-    fp32 const pi = 3.1415926535897932384626433832795f;
     fp32 const h = 6.62607015E-34f;
-    fp32 const h_reduced = h / (2.0f * pi);
+    fp32 const h_reduced = h / (2.0f * std::numbers::pi_v<fp32>);
 
     fp32 const c = 299792458.0f;
 
     fp32 const eps0 = 8.8541878128E-12f;
 
-    fp32 const qp = sqrtf(4.0f * pi * eps0 * h_reduced * c);
+    fp32 const qp = sqrtf(4.0f * std::numbers::pi_v<fp32> * eps0 * h_reduced * c);
 
     std::cout << std::setprecision(10) << "Planck electric charge (fp32): " << qp << std::endl;
 
