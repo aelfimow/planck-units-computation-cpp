e9b4aa65dfa7278e9645fcb852110a47a11910c2
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-10 18:20:32 +0100

    Planck-electric-charge: use pi from numbers

--- Planck-electric-charge/fp64/main.cpp
+++ Planck-electric-charge/fp64/main.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <numbers>
 
 using fp64 = double;
 static_assert(sizeof(fp64) == 8U);
 
 int main(int, char *[])
 {
-    fp64 const pi = 3.1415926535897932384626433832795;
     fp64 const h = 6.62607015E-34;
-    fp64 const h_reduced = h / (2.0 * pi);
+    fp64 const h_reduced = h / (2.0 * std::numbers::pi_v<fp64>);
 
     fp64 const c = 299792458.0;
 
     fp64 const eps0 = 8.8541878128E-12;
 
-    fp64 const qp = sqrt(4.0 * pi * eps0 * h_reduced * c);
+    fp64 const qp = sqrt(4.0 * std::numbers::pi_v<fp64> * eps0 * h_reduced * c);
 
     std::cout << std::setprecision(10) << "Planck electric charge (fp64): " << qp << std::endl;
 
