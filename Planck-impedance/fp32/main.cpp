60002d59a91f49b1dec8d1085c405bd782dfb865
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-10 18:20:33 +0100

    Planck-impedance: use pi from numbers

--- Planck-impedance/fp32/main.cpp
+++ Planck-impedance/fp32/main.cpp
@@ -2,19 +2,18 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <numbers>
 
 using fp32 = float;
 static_assert(sizeof(fp32) == 4U);
 
 int main(int, char *[])
 {
-    fp32 const pi = 3.1415926535897932384626433832795f;
-
     fp32 const c = 299792458.0f;
 
     fp32 const eps0 = 8.8541878128E-12f;
 
-    fp32 const Zp = 1.0f / (4.0f * pi * eps0 * c);
+    fp32 const Zp = 1.0f / (4.0f * std::numbers::pi_v<fp32> * eps0 * c);
 
     std::cout << std::setprecision(10) << "Planck impedance (fp32): " << Zp << std::endl;
 
