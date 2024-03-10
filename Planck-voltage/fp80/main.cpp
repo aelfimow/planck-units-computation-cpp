89d37f3b48cb1d6e45e3ac9c6339382a966c2808
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-10 18:20:37 +0100

    Planck-voltage: use pi from numbers

--- Planck-voltage/fp80/main.cpp
+++ Planck-voltage/fp80/main.cpp
@@ -2,21 +2,20 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <numbers>
 
 using fp80 = long double;
 static_assert(sizeof(fp80) >= 10U);
 
 int main(int, char *[])
 {
-    fp80 const pi = 3.1415926535897932384626433832795L;
-
     fp80 const c = 299792458.0L;
 
     fp80 const eps0 = 8.8541878128E-12L;
 
     fp80 const G = 6.67430E-11L;
 
-    fp80 const Up = sqrtl(powl(c, 4.0L) / (4.0L * pi * eps0 * G));
+    fp80 const Up = sqrtl(powl(c, 4.0L) / (4.0L * std::numbers::pi_v<fp80> * eps0 * G));
 
     std::cout << std::setprecision(10) << "Planck voltage (fp80): " << Up << std::endl;
 
