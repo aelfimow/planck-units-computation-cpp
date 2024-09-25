20a23378f6263175b3760dfd33124602fe497ba6
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-25 20:05:18 +0200

    Planck-angular-frequency/fp32: added file

---
+++ Planck-angular-frequency/fp32/Constants.h
@@ -0,0 +1,10 @@
+#ifdef Constants_h
+#error Already included.
+#else
+#define Constants_h
+
+extern fp32 Reduced_Planck_constant();
+extern fp32 Gravitational_constant();
+extern fp32 Speed_of_light();
+
+#endif
