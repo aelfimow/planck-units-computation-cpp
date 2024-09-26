6014eafd526d91cdf52593054318ca28804dcc05
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-26 20:34:34 +0200

    Planck-angular-frequency/fp64: use new functions

--- Planck-angular-frequency/fp64/main.cpp
+++ Planck-angular-frequency/fp64/main.cpp
@@ -2,19 +2,18 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
-#include <numbers>
 
-using fp64 = double;
-static_assert(sizeof(fp64) == 8U);
+#include "fp64.h"
+#include "Constants.h"
+
 
 int main(int, char *[])
 {
-    fp64 const h = 6.62607015E-34;
-    fp64 const h_reduced = h / (2.0 * std::numbers::pi_v<fp64>);
+    fp64 const h_reduced = Reduced_Planck_constant();
 
-    fp64 const G = 6.67430E-11;
+    fp64 const G = Gravitational_constant();
 
-    fp64 const c = 299792458.0;
+    fp64 const c = Speed_of_light();
 
     fp64 const omega = sqrt(pow(c, 5.0) / (h_reduced * G));
 
