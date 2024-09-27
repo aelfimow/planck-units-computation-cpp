508c6ef1c3e5e4c740396ab1da77eef49a80744f
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-27 15:18:37 +0200

    Planck-angular-frequency/fp80: added file

---
+++ Planck-angular-frequency/fp80/Constants.h
@@ -0,0 +1,10 @@
+#ifdef Constants_h
+#error Already included.
+#else
+#define Constants_h
+
+extern fp80 Reduced_Planck_constant();
+extern fp80 Gravitational_constant();
+extern fp80 Speed_of_light();
+
+#endif
