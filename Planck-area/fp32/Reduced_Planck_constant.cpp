79a727229c9e3f42234a02d4c8f6653eaed344e4
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-29 18:49:52 +0200

    Planck-area/fp32: added file

---
+++ Planck-area/fp32/Reduced_Planck_constant.cpp
@@ -0,0 +1,12 @@
+#include "fp32.h"
+#include "Constants.h"
+
+#include <numbers>
+
+fp32 Reduced_Planck_constant()
+{
+    fp32 const h = 6.62607015E-34f;
+    fp32 const h_reduced = h / (2.0f * std::numbers::pi_v<fp32>);
+
+    return h_reduced;
+}
