92b3ded0f8f8fe2eee3b5b7339a84696235c6bd6
Alexander Elfimow <my.free.mail@gmx.de>
2024-10-06 19:22:06 +0200

    Planck-area/fp128: added file

---
+++ Planck-area/fp128/Constants.h
@@ -0,0 +1,10 @@
+#ifdef Constants_h
+#error Already included.
+#else
+#define Constants_h
+
+extern fp128 Reduced_Planck_constant();
+extern fp128 Gravitational_constant();
+extern fp128 Speed_of_light();
+
+#endif
